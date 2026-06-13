52f67b821560099d3a04b9d9b004879f0d6c8553
fbouyerguerin-hub <f.bouyer.guerin@gmail.com>
2026-06-13 19:05:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE_NAME = 'chantier-app-v60';
+const CACHE_NAME = 'chantier-app-v61';
 const ASSETS = [
   '/Chantier-APP/chantier-app.html',
   '/Chantier-APP/manifest.json',
 ];
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
